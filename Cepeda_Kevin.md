f5c1f30fc0b7c6aefd696ae8c74f133b4f82fb0f
CyborgK27 <slenderstalin@hotmail.com>
2023-11-24 11:59:54 -0500

    add pages home, coupons, cart, profil and nabvar

---
+++ Cepeda_Kevin.md
@@ -0,0 +1,6 @@
+# Desarrollo de aplicaciones web avanzada
+## de: Cepeda Yupanqui Kevin Stalin
+
+### Evidencias y links:
+- Maqueta de Figma:
+https://www.figma.com/proto/DEUvB4RGbfPCft0Uyii8JU/my-project?type=design&node-id=1-6&t=Ug7fvu2OaF8eX98Q-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A6&mode=design
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,18 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'coupons',
+    loadChildren: () => import('./pages/coupons/coupons.module').then( m => m.CouponsPageModule)
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./pages/cart/cart.module').then( m => m.CartPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+  },
 ];
 
 @NgModule({
